d2054527f79fc78a53ef32048d0ccc84a82bf394
jimmycababa <jimmycababa@gmail.com>
2021-08-30 11:07:09 -0500

    added navbar with routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :kicks
+  get "favorites", to: "favorites#index"
+  root "kicks#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
