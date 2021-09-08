54cb611d3e4cd1e0984666a4291eb833d2e65549
jimmycababa <jimmycababa@gmail.com>
2021-09-07 23:45:13 -0500

    pairing session changes with notes

--- app/controllers/kicks_controller.rb
+++ app/controllers/kicks_controller.rb
@@ -4,7 +4,7 @@ class KicksController < ApplicationController
   # GET /kicks or /kicks.json
   # .all is an active record method that uses active record to talk to the database ORM (object relational mapper), which is the interface between the programming language and the database to communicate with each other.
   def index
-    @kicks = Kick.all.limit(3)
+    @kicks = Kick.all
   end
 
   # GET /kicks/1 or /kicks/1.json
@@ -22,56 +22,60 @@ class KicksController < ApplicationController
   end
 
   # POST /kicks or /kicks.json
-  def create  
+  def create
     @kick = Kick.new(kick_params)
 
     # respond_to do |format|
-      if @kick.save
-        redirect_to @kick, notice: "Kick was successfully created."
-        # format.html { redirect_to @kick, notice: "Kick was successfully created." }
-        # format.json { render :show, status: :created, location: @kick }
-      else
-        render :new, status: :unprocessable_entity 
+    if @kick.save
+      redirect_to @kick, notice: "Kick was successfully created."
+      # format.html { redirect_to @kick, notice: "Kick was successfully created." }
+      # format.json { render :show, status: :created, location: @kick }
+    else
+      render :new, status: :unprocessable_entity
 
-        # format.html { render :new, status: :unprocessable_entity }
-        # format.json { render json: @kick.errors, status: :unprocessable_entity }
-      end
+      # format.html { render :new, status: :unprocessable_entity }
+      # format.json { render json: @kick.errors, status: :unprocessable_entity }
+    end
     # end
   end
 
   # PATCH/PUT /kicks/1 or /kicks/1.json
   def update
     # respond_to do |format|
-      if @kick.update(kick_params)
-        redirect_to @kick, notice: "Kick was successfully updated."
-        # format.html { redirect_to @kick, notice: "Kick was successfully updated." }
-        # format.json { render :show, status: :ok, location: @kick }
-      else
-        render :edit, status: :unprocessable_entity
-        # format.html { render :edit, status: :unprocessable_entity }
-        # format.json { render json: @kick.errors, status: :unprocessable_entity }
-      end
+    # update is an active record method and calling kick_params is added security. we filtered the params only to what we want
+    if @kick.update(kick_params)
+      redirect_to @kick, notice: "Kick was successfully updated."
+      # format.html { redirect_to @kick, notice: "Kick was successfully updated." }
+      # format.json { render :show, status: :ok, location: @kick }
+    else
+      render :edit, status: :unprocessable_entity
+      # format.html { render :edit, status: :unprocessable_entity }
+      # format.json { render json: @kick.errors, status: :unprocessable_entity }
+    end
     # end
   end
 
   # DELETE /kicks/1 or /kicks/1.json
   def destroy
     @kick.destroy
+    # kicks_url is from the resources methods in routes file and is the index
     redirect_to kicks_url, notice: "Kick was successfully destroyed."
     # respond_to do |format|
-      # format.html { redirect_to kicks_url, notice: "Kick was successfully destroyed." }
-      # format.json { head :no_content }
+    # format.html { redirect_to kicks_url, notice: "Kick was successfully destroyed." }
+    # format.json { head :no_content }
     # end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_kick
-      @kick = Kick.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def kick_params
-      params.require(:kick).permit(:brand, :style, :size)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_kick
+    @kick = Kick.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def kick_params
+    # filtered list of parameters
+    params.require(:kick).permit(:brand, :style, :size)
+  end
 end
